ca2409529f64889a54611d6687f50dedd922c464
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-03-06 08:41:53 -0300

    phpcodigos

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $nome = "Matheus Wagatuma";
+    $idade = "16";
+    $endereco = "Rua Ivaipora, 65";
+    $cidade = "Jardim Alegre";
+    $cep = "86860-000";
+    $semestre1 = 10;
+    $semestre2 = 8;
+    $semestre3 = 9;
+    $media = ($semestre1+$semestre2+$semestre3)/3;
+
+    echo "seu nome é:". $nome;
+    echo ("</br>");
+    echo "sua idade é:". $idade;
+    echo ("</br>");
+    echo "seu endereço é:". $endereco;
+    echo ("</br>");
+    echo "sua cidade é:". $cidade;
+    echo ("</br>");
+    echo "seu cep é:". $cep;
+    echo ("</br>");
+    echo "sua nota no primeiro semestre é:". $semestre1;
+    echo ("</br>");
+    echo "sua nota no segundo semestre é:". $semestre2;
+    echo ("</br>");
+    echo "sua nota no terceiro semestre é:". $semestre3;
+    echo ("</br>");
+    echo ("</br>");
+    echo "sua média é:". $media
+?>
